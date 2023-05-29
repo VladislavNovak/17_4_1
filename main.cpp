e9bdbf8a886be016697b1a050e603c64e7065644
Novak <novaldix@gmail.com>
2023-05-29 10:03:17 +0300

    17_4_1 added solution

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
-int main() {
+using std::cout;
+using std::endl;
 
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int arrA[] = { 1, 2, 3 };
+    int arrB[] = { 6, 7, 8 };
+
+    cout << "Before:\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << *(arrA + i) << " - " << *(arrB + i) << endl;
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        swap(*(arrA + i), *(arrB + i));
+    }
+
+    cout << "After:\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << *(arrA + i) << " - " << *(arrB + i) << endl;
+    }
 }
